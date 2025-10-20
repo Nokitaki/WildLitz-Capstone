e017173ee0cf751f5904fa9be066c9d39768eeaa
Nokitaki <165502987+Nokitaki@users.noreply.github.com>
2025-10-21 03:04:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Navigate to Django backend folder
+echo "=== DEBUG: Current directory ==="
+pwd
+echo "=== DEBUG: Listing contents ==="
+ls -R
+
+# Move into Django backend folder
 cd backend/wildlitz
+
+echo "=== DEBUG: Inside backend/wildlitz ==="
+pwd
+ls
 
 # Upgrade pip
 python -m pip install --upgrade pip
 
-# Install backend dependencies
-pip install -r ../../requirements.txt
+# Use absolute path to requirements.txt
+pip install -r /opt/render/project/src/requirements.txt
 
 # Collect static files and run migrations
 python manage.py collectstatic --noinput
