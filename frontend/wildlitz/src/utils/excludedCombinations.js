de24f458c02e7ca937612663fda2df583112d2db
Spencer Nacario <spencernacario@gmail.com>
2025-12-08 11:27:29 +0800

    Add Sloth animal and update excluded combinations
    Added the Sloth to animals_to_add.json with 'l' as the target sound in the jungle environment. Also excluded the 'p' sound in any position for the jungle environment in excludedCombinations.js.

--- frontend/wildlitz/src/utils/excludedCombinations.js
+++ frontend/wildlitz/src/utils/excludedCombinations.js
@@ -21,6 +21,7 @@ export const EXCLUDED_ENVIRONMENT_COMBINATIONS = [
   ['c', 'middle', 'savanna'],
   ['d', 'anywhere', 'jungle'],
   ['k', 'anywhere', 'jungle'],
+  ['p', 'anywhere', 'jungle'],
 ];
 
 export const CORE_SOUNDS = ['g', 'k', 'w', 'd', 'r', 'c', 'h', 's', 'm', 't', 'b', 'p', 'f', 'l', 'z'];
