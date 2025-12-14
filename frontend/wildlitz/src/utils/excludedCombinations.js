dc9c4e02b4ac1b85e74238bcfd3e1cb5135d78eb
Spencer Nacario <spencernacario@gmail.com>
2025-12-14 10:19:36 +0800

    Add new excluded combination for 'l' in middle ocean
    Added ['l', 'middle', 'ocean'] to EXCLUDED_ENVIRONMENT_COMBINATIONS to prevent this specific combination from being used.

--- frontend/wildlitz/src/utils/excludedCombinations.js
+++ frontend/wildlitz/src/utils/excludedCombinations.js
@@ -20,6 +20,7 @@ export const EXCLUDED_ENVIRONMENT_COMBINATIONS = [
   ['r', 'anywhere', 'jungle'],
   ['m', 'middle', 'ocean'],
   ['g', 'ending', 'ocean'],
+  ['l', 'middle', 'ocean'],
 ];
 
 export const CORE_SOUNDS = ['g', 'k', 'w', 'd', 'r', 'c', 'h', 's', 'm', 't', 'b', 'p', 'f', 'l', 'z'];
